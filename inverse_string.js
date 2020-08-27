14d5a8e03047799859598972061803f5d0a6e76e
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-08-27 12:15:38 -0500

    Update inverse_string.js

--- inverse_string.js
+++ inverse_string.js
@@ -1,5 +1,5 @@
 function inverse(str){
-   //var a = str.split("");
-   return str.split("").reverse().join("");
+   //var a = str.split(""); //detache caractere separe par ""
+   return str.split("").reverse().join(""); // retourne caractere separe par "", renverse et join ces caracteres.
  }
 console.log (inverse("Patrick"));
